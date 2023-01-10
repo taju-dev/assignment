69fef3563e7f67898be840066f7f1b6c95663e15
Tajuddin alam <alamtajuddin2@gmail.com>
2023-01-10 23:00:27 +0530

    initial commit

---
+++ 1.2/index.js
@@ -0,0 +1,7 @@
+let val = document.getElementById("demo"); // target html tag by id
+
+// creating a function where onclick event is set to trigger
+
+function myfunction() {
+  val.innerHTML = "implementation of getElementById";
+}
---
+++ 8.2/prime.js
@@ -0,0 +1,18 @@
+let number = 5;
+let isPrime = true;
+
+if (number === 1) {
+  console.log("1 is neither prime nor composite number.");
+} else if (number > 1) {
+  for (let i = 2; i < number; i++) {
+    if (number % i == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+}
+if (isPrime) {
+  console.log(`${number} is a prime number`);
+} else {
+  console.log(`${number} is a not prime number`);
+}
